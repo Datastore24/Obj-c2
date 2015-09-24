2bd0aeab2bbf4e9e0e50907b67fd64eaeb56298d
datastore24 <co@datastore24.ru>
2015-09-24 11:18:16 +0300

    Lesson4 Home Work

---
+++ LessonII-4HW/LessonII-4HW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LessonII-4HW
+//
+//  Created by Кирилл Ковыршин on 24.09.15.
+//  Copyright © 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LessonII-4HW/LessonII-4HW/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LessonII-4HW
+//
+//  Created by Кирилл Ковыршин on 24.09.15.
+//  Copyright © 2015 datastore24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
